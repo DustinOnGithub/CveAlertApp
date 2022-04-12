5b553aee354d9ca889b46d447ebc7df11aa7cca3
DustinOnGithub <dp.heise@gmail.com>
2022-04-12 16:56:12 +0100

    cleanup

--- app/src/main/java/com/example/cvealert/MainActivity.kt
+++ app/src/main/java/com/example/cvealert/MainActivity.kt
@@ -1,28 +1,14 @@
 package com.example.cvealert
 
-import android.content.Context
 import android.os.Bundle
-import android.util.AttributeSet
 import android.util.Log
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationMenuView
 import com.google.android.material.bottomnavigation.BottomNavigationView
-
-//import com.example.cvealert.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
-    private lateinit var drawerLayout: DrawerLayout
-    private val timelineFragment: TimelineFragment = TimelineFragment()
-    private val subscriptionsFragment: SubscriptionsFragment = SubscriptionsFragment()
-    private val settingsFragment: SettingsFragment = SettingsFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/cvealert/SettingsFragment.kt
+++ app/src/main/java/com/example/cvealert/SettingsFragment.kt
@@ -5,9 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.Toast
-import androidx.databinding.DataBindingUtil
 
 class SettingsFragment : Fragment() {
 
