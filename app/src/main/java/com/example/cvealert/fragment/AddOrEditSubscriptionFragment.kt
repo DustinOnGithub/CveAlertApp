b12b13e4856de9910032f4340625781636e89d51
DustinOnGithub <dp.heise@gmail.com>
2022-04-15 18:09:48 +0100

    renamed AddSubscriptionFragment.kt to AddOrEditSubscriptionFragment.kt; dirty add_or_edit_subscription layout

--- app/src/main/java/com/example/cvealert/fragment/AddOrEditSubscriptionFragment.kt
+++ app/src/main/java/com/example/cvealert/fragment/AddOrEditSubscriptionFragment.kt
@@ -7,13 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.cvealert.R
 
-class AddSubscriptionFragment : Fragment() {
+class AddOrEditSubscriptionFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_subscription, container, false)
+        return inflater.inflate(R.layout.fragment_add_or_edit_subscription, container, false)
     }
 }
