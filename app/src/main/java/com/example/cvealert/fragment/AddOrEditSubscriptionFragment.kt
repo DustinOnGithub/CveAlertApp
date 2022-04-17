06bf7dae42b628d23ecf7bd0a669b73eb731f314
DustinOnGithub <dp.heise@gmail.com>
2022-04-17 17:34:43 +0100

    display cpe-string

--- app/src/main/java/com/example/cvealert/fragment/AddOrEditSubscriptionFragment.kt
+++ app/src/main/java/com/example/cvealert/fragment/AddOrEditSubscriptionFragment.kt
@@ -1,6 +1,9 @@
 package com.example.cvealert.fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -22,14 +25,15 @@ class AddOrEditSubscriptionFragment : Fragment() {
     private lateinit var cpeVersionET: EditText
     private lateinit var pushUpNotificationCB: CheckBox
     private lateinit var isActiveCB: CheckBox
-
+    private lateinit var cpeStringTV: TextView
+    private lateinit var saveSubscriptionBtn: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_add_or_edit_subscription, container, false)
-        val saveSubscriptionBtn = view.findViewById<Button>(R.id.saveSubscriptionBtn)
+        saveSubscriptionBtn = view.findViewById<Button>(R.id.saveSubscriptionBtn)
         myViewModel = ViewModelProvider(this).get(MyViewModel::class.java)
 
         cpePartRG = view.findViewById(R.id.HardOrSoftwareRG)
@@ -39,20 +43,73 @@ class AddOrEditSubscriptionFragment : Fragment() {
         cpeVersionET = view.findViewById(R.id.versionET)
         pushUpNotificationCB = view.findViewById(R.id.pushUpCB)
         isActiveCB = view.findViewById(R.id.isActiveCB)
+        cpeStringTV = view.findViewById(R.id.cpeStringTV)
 
         clearInputs()
-
-        //todo: onchange listener
+        updateCpeTV()
 
         saveSubscriptionBtn.text = resources.getString(R.string.add)
-        saveSubscriptionBtn.setOnClickListener {
-            saveSubscription(view)
-        }
+
+        bindListener()
 
         return view
     }
 
-    private fun saveSubscription(view: View) {
+    private fun bindListener() {
+
+        cpePartRG.setOnCheckedChangeListener { _, _ -> updateCpeTV() }
+        isActiveCB.setOnCheckedChangeListener { _, _ -> updateCpeTV() }
+        pushUpNotificationCB.setOnCheckedChangeListener { _, _ -> updateCpeTV() }
+        cpeVendorET.afterTextChanged { updateCpeTV() }
+        cpeProductET.afterTextChanged { updateCpeTV() }
+        cpeVersionET.afterTextChanged { updateCpeTV() }
+        cpeUpdateET.afterTextChanged { updateCpeTV() }
+
+        saveSubscriptionBtn.setOnClickListener { saveSubscription() }
+    }
+
+    private fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
+        this.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(editable: Editable?) {
+                afterTextChanged.invoke(editable.toString())
+            }
+        })
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateCpeTV() {
+
+        val cpeVersion = "cpe:2.3.:"
+
+        val part = when (getSelectedPart()) {
+            Part.APPLICATIONS -> "a"
+            Part.UNDEFINED -> "*"
+            Part.HARDWARE -> "h"
+            else -> "o"
+        }
+
+        val vendor = if (cpeVendorET.text.isEmpty()) "*" else cpeVendorET.text
+        val product = if (cpeProductET.text.isEmpty()) "*" else cpeProductET.text
+        val version = if (cpeVersionET.text.isEmpty()) "*" else cpeVersionET.text
+        val update = if (cpeUpdateET.text.isEmpty()) "*" else cpeUpdateET.text
+        val edition = "*"
+        val language = "*"
+        val swEdition = "*"
+        val targetSoftware = "*"
+        val targetHardware = "*"
+        val other = "*"
+
+        cpeStringTV.text =
+            "${cpeVersion}:${part}:${vendor}:${product}:${version}:${update}:${edition}:${language}:${swEdition}:$targetSoftware:$targetHardware:$other"
+    }
+
+    private fun saveSubscription() {
 
         //todo: add update functionality
 
