1625191e2e661710c8859fd072947577b84eeba3
DustinOnGithub <dp.heise@gmail.com>
2022-04-29 11:25:34 +0100

    nice layout for cve-detail-view

--- app/src/main/java/com/example/cvealert/fragment/cveTimeline/CveDetailFragment.kt
+++ app/src/main/java/com/example/cvealert/fragment/cveTimeline/CveDetailFragment.kt
@@ -1,5 +1,6 @@
 package com.example.cvealert.fragment.cveTimeline
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.navArgs
 import com.example.cvealert.Cpe
 import com.example.cvealert.R
@@ -21,23 +23,47 @@ class CveDetailFragment : Fragment() {
     ): View? {
 
         val view = inflater.inflate(R.layout.fragment_cve_detail, container, false)
+        val cve = args.selectedCVE.cve
+        val subscription = args.selectedCVE.subscription
 
-        view.findViewById<TextView>(R.id.titleTv).text = args.selectedCVE.cve.cve
-        view.findViewById<TextView>(R.id.cvssV2Tv).text =
-            args.selectedCVE.cve.cvssV2Score.toString()
-        view.findViewById<TextView>(R.id.cvssV3TV).text =
-            args.selectedCVE.cve.cvssV3Score.toString()
-        view.findViewById<TextView>(R.id.cvssV2StringTv).text = args.selectedCVE.cve.cvssV2String
-        view.findViewById<TextView>(R.id.cvssV3String).text = args.selectedCVE.cve.cvssV3String
-        view.findViewById<TextView>(R.id.cveDescriptionTV).text = args.selectedCVE.cve.description
-        view.findViewById<TextView>(R.id.publishedDateTv).text = args.selectedCVE.cve.publishedDate
-        view.findViewById<TextView>(R.id.lastModifiedTv).text =
-            args.selectedCVE.cve.lastModifiedDate
+        view.findViewById<TextView>(R.id.titleTv).text = cve.cve
+        view.findViewById<TextView>(R.id.cvssV2Tv).text = cve.cvssV2Score.toString()
+        view.findViewById<TextView>(R.id.cvssV3TV).text = cve.cvssV3Score.toString()
+        view.findViewById<TextView>(R.id.cvssV2StringTv).text = cve.cvssV2String
+        view.findViewById<TextView>(R.id.cvssV3String).text = cve.cvssV3String
+        view.findViewById<TextView>(R.id.cveDescriptionTV).text = cve.description
+        view.findViewById<TextView>(R.id.publishedDateTv).text = cve.publishedDate
+        view.findViewById<TextView>(R.id.lastModifiedTv).text = cve.lastModifiedDate
         view.findViewById<TextView>(R.id.subscriptionStringTv).text =
-            Cpe.generateStringFromSubscription(args.selectedCVE.subscription)
+            Cpe.generateStringFromSubscription(subscription)
         view.findViewById<Button>(R.id.linkToNvdBt).text = "Link to NVD"
+        view.findViewById<TextView>(R.id.cveDetailVendorTv).text = subscription.vendor
+        view.findViewById<TextView>(R.id.cveDetailproductTv).text = subscription.product
 
+        view.findViewById<TextView>(R.id.cvssV3TV).setTextColor(
+            ContextCompat.getColor(
+                requireContext(),
+                when (cve.cvssV3Severity) {
+                    "CRITICAL" -> R.color.cvssCritical
+                    "HIGH" -> R.color.cvssHigh
+                    "MEDIUM" -> R.color.cvssMedium
+                    "LOW" -> R.color.cvssLow
+                    else -> Color.BLACK
+                }
+            )
+        )
 
+        view.findViewById<TextView>(R.id.cvssV2Tv).setTextColor(
+            ContextCompat.getColor(
+                requireContext(),
+                when (cve.cvssV3Severity) {
+                    "HIGH" -> R.color.cvssHigh
+                    "MEDIUM" -> R.color.cvssMedium
+                    "LOW" -> R.color.cvssLow
+                    else -> Color.BLACK
+                }
+            )
+        )
 
         return view
     }
