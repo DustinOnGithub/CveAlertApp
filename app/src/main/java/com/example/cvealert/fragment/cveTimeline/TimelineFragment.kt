fd25b95ed4f2330e34bff6b87c7eb86fedf5d405
DustinOnGithub <dp.heise@gmail.com>
2022-05-01 15:22:07 +0100

    when no CVEs available or subscription: display placeholder text

--- app/src/main/java/com/example/cvealert/fragment/cveTimeline/TimelineFragment.kt
+++ app/src/main/java/com/example/cvealert/fragment/cveTimeline/TimelineFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -28,6 +29,13 @@ class TimelineFragment : Fragment() {
 
         myViewModelDb = ViewModelProvider(this)[MyViewModelDb::class.java]
         myViewModelDb.selectCVEwithSubscriptionAndCPEs.observe(viewLifecycleOwner) { cves ->
+
+            if (cves.isEmpty()) {
+                view.findViewById<TextView>(R.id.noDataAvailableTV).visibility = View.VISIBLE
+            } else {
+                view.findViewById<TextView>(R.id.noDataAvailableTV).visibility = View.INVISIBLE
+            }
+
             adapter.setData(cves, requireContext())
         }
 
