ebfe6ae54a73f014b4cc86fcbbbf451281afaf4c
DustinOnGithub <dp.heise@gmail.com>
2022-05-02 20:07:09 +0100

    added actionBar

--- app/src/main/java/com/example/cvealert/MainActivity.kt
+++ app/src/main/java/com/example/cvealert/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.cvealert
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import androidx.work.Constraints
 import androidx.work.NetworkType
@@ -26,6 +29,22 @@ class MainActivity : AppCompatActivity() {
         val bottomNavigationMenuView = findViewById<BottomNavigationView>(R.id.myNavHostFragment)
         bottomNavigationMenuView.setupWithNavController(navController)
 
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        NavigationUI.setupActionBarWithNavController(
+            this,
+            navController,
+            appBarConfiguration
+        )
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            onBackPressed()
+            return true
+        }
+        return true
     }
 
     private fun enqueueWorkers() {
