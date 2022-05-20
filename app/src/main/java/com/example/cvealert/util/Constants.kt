793b41b27eade2ea082bc0163294aa64e14f3d82
DustinOnGithub <dp.heise@gmail.com>
2022-05-20 11:32:13 +0100

    mini refactor

--- app/src/main/java/com/example/cvealert/util/Constants.kt
+++ app/src/main/java/com/example/cvealert/util/Constants.kt
@@ -12,9 +12,10 @@ class Constants {
         const val WORKER_REPEAT_INTERVAL: Long = 60 //minutes
         const val NO_CVSS_SCORE: Float = (-1).toFloat()
         const val RESULTS_PER_PAGE = 100
+        private const val MONTHS_BACK: Long = 1
 
         fun getLastCVEDateTime(): String {
-            return ZonedDateTime.now(TimeZone.getDefault().toZoneId()).minusMonths(1)
+            return ZonedDateTime.now(TimeZone.getDefault().toZoneId()).minusMonths(MONTHS_BACK)
                 .format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss:SSS 'UTC'xxx"))
         }
     }
